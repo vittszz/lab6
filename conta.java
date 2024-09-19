22093b6a0cc84e9340f21fea6394f313a35d5719
João Victor Cara <144183219+vittszz@users.noreply.github.com>
2024-09-19 20:08:17 -0300

    Create conta.java

---
+++ conta.java
@@ -0,0 +1,36 @@
+public class Conta {
+    private int numero;
+    private String titular;
+    private double saldo;
+
+    public Conta(int numero, String titular, double saldo) {
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    // Getters e setters
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
